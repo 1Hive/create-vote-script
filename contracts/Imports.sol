8b6b97e732c771929f7f045b8fc4b91e2ef1f358
William Griffiths <willjgriff@gmail.com>
2021-06-11 13:44:31 +0100

    Better scripts

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "@1hive/apps-token-manager/contracts/HookedTokenManager.sol";
+import "@aragon/apps-voting-disputable/contracts/DisputableVoting.sol";
 import "@aragon/apps-vault/contracts/Vault.sol";
 import "@aragon/os/contracts/acl/ACL.sol";
 
